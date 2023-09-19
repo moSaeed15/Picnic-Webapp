81aa9d01dc2fb7d6d219459a90db79eef3ba2556
moSaeed05 <m.elsaeed@ZINAD-EXT.NET>
2023-09-19 23:02:15 +0300

    intial file creation

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="font-Inter">
+      <nav>
+        <img src="/logo.png" alt="picnic-logo" loading="lazy" />
+        <div className="">
+          <img src="" alt="Notification Icon" loading="lazy" />
+          <div className="">
+            <img src="" alt="" loading="lazy" />
+            <span>John</span>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
